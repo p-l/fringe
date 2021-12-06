6103c8dc289335210e5984e38ceecb9a348bf743
Pierre-Luc Simard <pierreluc.simard@gmail.com>
2021-12-06 17:51:23 -0500

    Transform db to repositories and add tests (#2)

---
+++ internal/repositories/user_repository.go
@@ -0,0 +1,226 @@
+package repositories
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/jmoiron/sqlx"
+)
+
+// UserRepository stores and access data in a sqlite database.
+type UserRepository struct {
+	db *sqlx.DB
+}
+
+type User struct {
+	Email        string `db:"email"`
+	PasswordHash string `db:"password"`
+	CreatedAt    int64  `db:"created_at"`
+	UpdatedAt    int64  `db:"updated_at"`
+	LastSeenAt   int64  `db:"last_seen_at"`
+}
+
+var (
+	ErrUserNotFound     = errors.New("queried user could not be not found")
+	ErrUserAlreadyExist = errors.New("user with same email already exist in database")
+)
+
+// NewUserRepository returns a ready to use UserRepository with a new database connexion.
+func NewUserRepository(db *sqlx.DB) (*UserRepository, error) {
+	if err := createUserTable(db); err != nil {
+		return nil, err
+	}
+
+	return &UserRepository{db: db}, nil
+}
+
+func createUserTable(db *sqlx.DB) error {
+	createTx := db.MustBegin()
+	defer func() { _ = createTx.Rollback() }()
+
+	createTx.MustExec("CREATE TABLE IF NOT EXISTS users (" +
+		"email string NOT NULL, " +
+		"password string NOT NULL, " +
+		"created_at int64, " +
+		"updated_at int64, " +
+		"last_seen_at int64)")
+	createTx.MustExec("CREATE INDEX IF NOT EXISTS idx_users_email ON users (email)")
+
+	if err := createTx.Commit(); err != nil {
+		return fmt.Errorf("cannot create users table: %w", err)
+	}
+
+	return nil
+}
+
+func CreatePasswordHash(password string) (string, error) {
+	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	if err != nil {
+		return "", fmt.Errorf("creting password hash failed: %w", err)
+	}
+
+	return hash, nil
+}
+
+func (u *User) PasswordMatch(password string) bool {
+	valid, err := argon2id.ComparePasswordAndHash(password, u.PasswordHash)
+	if err != nil {
+		log.Printf("could not validte password hash: %v", err)
+
+		return false
+	}
+
+	return valid
+}
+
+// UserWithEmail Looks for a user record that matches the provided email and returns a pointer to User stuck if found or nil if not.
+func (r *UserRepository) UserWithEmail(email string) (user *User, err error) {
+	var u User
+	if err := r.db.Get(&u, "SELECT email,password,created_at,updated_at,last_seen_at FROM users WHERE email == $1 LIMIT 1", email); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrUserNotFound
+		}
+
+		return nil, fmt.Errorf("could not retrieve user %s: %w", email, err)
+	}
+
+	return &u, nil
+}
+
+// CreateUser INSERT a new user record with email and argon2id password hash from the provided password.
+// If the user already exists returns the record from the Database, otherwise return the newly created User.
+func (r *UserRepository) CreateUser(email string, password string) (*User, error) {
+	dbUser, err := r.UserWithEmail(email)
+	if !errors.Is(err, ErrUserNotFound) && err != nil {
+		return nil, err
+	}
+
+	// Return user if it already exists
+	if dbUser != nil {
+		return dbUser, ErrUserAlreadyExist
+	}
+
+	// Create User
+	hash, err := CreatePasswordHash(password)
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	newUser := User{email, hash, now.Unix(), now.Unix(), now.Unix()}
+
+	insertTx, err := r.db.Begin()
+	if err != nil {
+		return nil, fmt.Errorf("could not create user %s: %w", email, err)
+	}
+	defer func() { _ = insertTx.Rollback() }() //nolint:wsl
+
+	// Insert record in the database
+	insert, err := insertTx.Prepare("INSERT INTO users (email, password, created_at, updated_at, last_seen_at) VALUES ($1,$2,$3,$4,$5)")
+	if err != nil {
+		return nil, fmt.Errorf("could not create user %s: %w", email, err)
+	}
+	defer insert.Close()
+
+	_, err = insert.Exec(newUser.Email, newUser.PasswordHash, newUser.CreatedAt, newUser.UpdatedAt, newUser.LastSeenAt)
+	if err != nil {
+		return nil, fmt.Errorf("could not create user %s: %w", email, err)
+	}
+
+	if err := insertTx.Commit(); err != nil {
+		return nil, fmt.Errorf("could not create user %s: %w", email, err)
+	}
+
+	return &newUser, nil
+}
+
+// UpdateUserPassword replaces the specified user's (found by email address) by the password provided.
+// The password is not stored as is. It is hashed with argon2id.
+func (r *UserRepository) UpdateUserPassword(email string, password string) (bool, error) {
+	hash, err := CreatePasswordHash(password)
+	if err != nil {
+		return false, err
+	}
+
+	updateTx, err := r.db.Begin()
+	if err != nil {
+		return false, fmt.Errorf("could not update %s password: %w", email, err)
+	}
+	defer func() { _ = updateTx.Rollback() }() //nolint:wsl
+
+	now := time.Now()
+
+	// Insert or update record in the database
+	stmt, err := updateTx.Prepare("UPDATE users SET password = $1, updated_at = $2, last_seen_at = $3 WHERE email == $4")
+	if err != nil {
+		return false, fmt.Errorf("could not update %s password: %w", email, err)
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(hash, now.Unix(), now.Unix(), email)
+	if err != nil {
+		return false, fmt.Errorf("could not update %s password: %w", email, err)
+	}
+
+	err = updateTx.Commit()
+	if err != nil {
+		return false, fmt.Errorf("could not update %s password: %w", email, err)
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+
+	return rowsAffected >= 1, nil
+}
+
+// TouchUser updates user's last_seen_at value with current Unix time.
+func (r *UserRepository) TouchUser(email string) error {
+	now := time.Now()
+
+	updateTx, err := r.db.Begin()
+	if err != nil {
+		return fmt.Errorf("could not update %s last_seen_at: %w", email, err)
+	}
+	defer func() { _ = updateTx.Rollback() }() //nolint:wsl
+
+	update, err := updateTx.Prepare("UPDATE users SET last_seen_at = $1 WHERE email == $2")
+	if err != nil {
+		return fmt.Errorf("could not update %s last_seen_at: %w", email, err)
+	}
+	defer update.Close()
+
+	result, err := update.Exec(now.Unix(), email)
+	if err != nil {
+		return fmt.Errorf("could not update %s last_seen_at: %w", email, err)
+	}
+
+	err = updateTx.Commit()
+	if err != nil {
+		return fmt.Errorf("could not update %s last_seen_at: %w", email, err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("could not update %s last_seen_at: %w", email, err)
+	}
+
+	if rowsAffected != 1 {
+		return ErrUserNotFound
+	}
+
+	return nil
+}
+
+// AuthenticateUser validates if the email and password combination matches an existing user
+// in the database with a password resulting in the same password hash.
+func (r *UserRepository) AuthenticateUser(email string, password string) (bool, error) {
+	user, err := r.UserWithEmail(email)
+	if err != nil {
+		return false, err
+	}
+
+	return user.PasswordMatch(password), nil
+}
---
+++ internal/repositories/user_repository_test.go
@@ -0,0 +1,336 @@
+package repositories_test
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jaswdr/faker"
+	"github.com/jmoiron/sqlx"
+	"github.com/p-l/fringe/internal/repositories"
+	"github.com/stretchr/testify/assert"
+)
+
+func dbOpen() (*sqlx.DB, sqlmock.Sqlmock) { //nolint:ireturn
+	mockDB, mockSQL, err := sqlmock.New()
+	if err != nil {
+		log.Panicf("FATAL: an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	db := sqlx.NewDb(mockDB, "sqlmock")
+
+	mockSQL.ExpectBegin()
+	mockSQL.ExpectExec("CREATE TABLE").WillReturnResult(sqlmock.NewResult(1, 1))
+	mockSQL.ExpectExec("CREATE INDEX").WillReturnResult(sqlmock.NewResult(1, 1))
+	mockSQL.ExpectCommit()
+
+	return db, mockSQL
+}
+
+func TestShouldReturnNewUserWhenCreating(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	mockSQL.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows([]string{"email", "password", "created_at", "updated_at", "last_seen_at"}))
+
+	mockSQL.ExpectBegin()
+	mockSQL.ExpectPrepare("INSERT INTO users").WillBeClosed()
+	mockSQL.ExpectExec("").WillReturnResult(sqlmock.NewResult(1, 1))
+	mockSQL.ExpectCommit()
+
+	userRepo, _ := repositories.NewUserRepository(db)
+	fake := faker.New()
+
+	email := fake.Internet().Email()
+	password := fake.Internet().Password()
+
+	user, err := userRepo.CreateUser(email, password)
+	if err != nil {
+		t.Errorf("Creating user failed: %v", err)
+	}
+
+	// CreateUser without an existing user must return a valid user with its hashed password
+	// The created user will have the provided password hash and will validate
+	assert.NotNil(t, user)
+
+	if user != nil {
+		assert.Equal(t, email, user.Email)
+		assert.NotEqual(t, password, user.PasswordHash)
+		assert.True(t, user.PasswordMatch(password))
+	}
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUpdateOnlyLastSeenWhenCreatingExistingUser(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+	password := fake.Internet().Password()
+	passwordHash, _ := repositories.CreatePasswordHash(password)
+	createdAt := fake.Time().Unix(time.Now())
+	updatedAt := fake.Time().Unix(time.Now())
+	lastSeenAt := fake.Time().Unix(time.Now())
+
+	newPassword := fake.Internet().Password()
+
+	// Return the fake User
+	mockSQL.ExpectQuery("SELECT").WillReturnRows(
+		sqlmock.NewRows([]string{"email", "password", "created_at", "updated_at", "last_seen_at"}).AddRow(email, passwordHash, createdAt, updatedAt, lastSeenAt))
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	user, err := userRepo.CreateUser(email, newPassword)
+	assert.ErrorIs(t, err, repositories.ErrUserAlreadyExist)
+
+	// CreatUser will return the existing user without modifying it
+	assert.NotNil(t, user)
+
+	if user != nil {
+		assert.Equal(t, email, user.Email)
+		// The retrieved user is expected to contain the fake password Hash and match
+		assert.True(t, user.PasswordMatch(password))
+		assert.Equal(t, createdAt, user.CreatedAt)
+		assert.Equal(t, updatedAt, user.UpdatedAt)
+		assert.Equal(t, lastSeenAt, user.LastSeenAt)
+	}
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUserWithEmailReturnsUnmodifiedUserWhenFound(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+	password := fake.Internet().Password()
+	passwordHash, _ := repositories.CreatePasswordHash(password)
+	createdAt := fake.Time().Unix(time.Now())
+	updatedAt := fake.Time().Unix(time.Now())
+	lastSeenAt := fake.Time().Unix(time.Now())
+
+	// Return the fake User
+	mockSQL.ExpectQuery("SELECT").WillReturnRows(
+		sqlmock.NewRows([]string{"email", "password", "created_at", "updated_at", "last_seen_at"}).AddRow(email, passwordHash, createdAt, updatedAt, lastSeenAt))
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	user, err := userRepo.UserWithEmail(email)
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	if user != nil {
+		assert.Equal(t, email, user.Email)
+		assert.Equal(t, passwordHash, user.PasswordHash)
+		assert.Equal(t, createdAt, user.CreatedAt)
+		assert.Equal(t, updatedAt, user.UpdatedAt)
+		assert.Equal(t, lastSeenAt, user.LastSeenAt)
+	}
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUserWithEmailReturnsErrorWhenUserNotFound(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+
+	// Return the fake User
+	mockSQL.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows([]string{"email", "password", "created_at", "updated_at", "last_seen_at"}))
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	user, err := userRepo.UserWithEmail(email)
+	assert.ErrorIs(t, err, repositories.ErrUserNotFound)
+	assert.Nil(t, user)
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUpdateUserPasswordFailsWithUnknownUser(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+	newPassword := fake.Internet().Password()
+
+	mockSQL.ExpectBegin()
+	mockSQL.ExpectPrepare("UPDATE users").WillBeClosed()
+	mockSQL.ExpectExec("").WillReturnResult(sqlmock.NewResult(0, 0))
+	mockSQL.ExpectCommit()
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	success, err := userRepo.UpdateUserPassword(email, newPassword)
+	assert.Nil(t, err)
+	assert.False(t, success)
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUpdateUserPasswordUpdatesKnownUser(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+	newPassword := fake.Internet().Password()
+
+	mockSQL.ExpectBegin()
+	// Ensure that password, updated_at AND last_seen_at are updated
+	mockSQL.ExpectPrepare("UPDATE users SET password = .*, updated_at = .*, last_seen_at = .* WHERE").WillBeClosed()
+	mockSQL.ExpectExec("").WillReturnResult(sqlmock.NewResult(0, 1))
+	mockSQL.ExpectCommit()
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	success, err := userRepo.UpdateUserPassword(email, newPassword)
+	assert.Nil(t, err)
+	assert.True(t, success)
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestTouchUserFailsWithUnknownUser(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+
+	mockSQL.ExpectBegin()
+	mockSQL.ExpectPrepare("UPDATE users").WillBeClosed()
+	mockSQL.ExpectExec("").WillReturnResult(sqlmock.NewResult(0, 0))
+	mockSQL.ExpectCommit()
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	err := userRepo.TouchUser(email)
+	assert.NotNil(t, err)
+	assert.ErrorIs(t, err, repositories.ErrUserNotFound)
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestTouchUserUpdatesOnlyKnownUser(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+
+	mockSQL.ExpectBegin()
+	// Ensures target "last_seen_at" column
+	mockSQL.ExpectPrepare("UPDATE users SET last_seen_at = .* WHERE").WillBeClosed()
+	mockSQL.ExpectExec("").WillReturnResult(sqlmock.NewResult(0, 1))
+	mockSQL.ExpectCommit()
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	err := userRepo.TouchUser(email)
+	assert.Nil(t, err)
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestAuthenticateReturnsUserNotFoundWhenNotFound(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+	password := fake.Internet().Password()
+
+	// Return the fake User
+	mockSQL.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows([]string{"email", "password", "created_at", "updated_at", "last_seen_at"}))
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	success, err := userRepo.AuthenticateUser(email, password)
+	assert.ErrorIs(t, err, repositories.ErrUserNotFound)
+	assert.False(t, success)
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestAuthenticateUserWithHAshWhenUserExists(t *testing.T) {
+	t.Parallel()
+
+	db, mockSQL := dbOpen()
+	defer db.Close()
+
+	fake := faker.New()
+	email := fake.Internet().Email()
+	password := fake.Internet().Password()
+	passwordHash, _ := repositories.CreatePasswordHash(password)
+	createdAt := fake.Time().Unix(time.Now())
+	updatedAt := fake.Time().Unix(time.Now())
+	lastSeenAt := fake.Time().Unix(time.Now())
+
+	// Return the fake User
+	mockSQL.ExpectQuery("SELECT").WillReturnRows(
+		sqlmock.NewRows([]string{"email", "password", "created_at", "updated_at", "last_seen_at"}).AddRow(email, passwordHash, createdAt, updatedAt, lastSeenAt))
+
+	userRepo, _ := repositories.NewUserRepository(db)
+
+	success, err := userRepo.AuthenticateUser(email, password)
+	assert.Nil(t, err)
+	assert.True(t, success)
+
+	// we make sure that all expectations were met
+	if err := mockSQL.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
