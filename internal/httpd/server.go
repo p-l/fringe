addcd64fa3bdebc9fcec67e26d7b4825eedae022
Pierre-Luc Simard <pl@mirego.com>
2021-12-17 20:37:38 -0500

    Rename home_handler to default_handler

--- internal/httpd/server.go
+++ internal/httpd/server.go
@@ -24,32 +24,34 @@ type Texts struct {
 const httpTimeouts = time.Second * 5
 
 // NewHTTPServer Create and configure the HTTP server.
-func NewHTTPServer(repo *repos.UserRepository, templates fs.FS, assets fs.FS, rootURL string, googleClientID string, googleClientSecret string, allowedDomain string, jwtSecret string, texts Texts) *http.Server {
+func NewHTTPServer(repo *repos.UserRepository, templates fs.FS, assets fs.FS, rootURL string, adminEmails []string, googleClientID string, googleClientSecret string, allowedDomain string, jwtSecret string, texts Texts) *http.Server {
 	googleOAuth := services.NewGoogleOAuthService(http.DefaultClient, googleClientID, googleClientSecret, fmt.Sprintf("%s/auth/google/callback", rootURL))
 
-	authHelper := helpers.NewAuthHelper(jwtSecret)
+	authHelper := helpers.NewAuthHelper(allowedDomain, jwtSecret, adminEmails)
 	pageHelper := helpers.NewPageHelper(templates)
 
 	logMiddleware := middlewares.NewLogMiddleware()
-	authMiddleware := middlewares.NewAuthMiddleware("/auth", []string{"/assets"}, authHelper)
+	authMiddleware := middlewares.NewAuthMiddleware("/auth/", []string{"/assets"}, authHelper)
 
-	homeHandler := handlers.NewHomeHandler(repo)
-	authHandler := handlers.NewAuthHandler(allowedDomain, googleOAuth, authHelper)
-	userHandler := handlers.NewUserHandler(repo, pageHelper, texts.PasswordHint, texts.PasswordInfoCardTitle, texts.PasswordInfoCardItems)
+	homeHandler := handlers.NewHomeHandler(repo, pageHelper)
+	authHandler := handlers.NewAuthHandler(googleOAuth, authHelper)
+	userHandler := handlers.NewUserHandler(repo, authHelper, pageHelper, texts.PasswordHint, texts.PasswordInfoCardTitle, texts.PasswordInfoCardItems)
 
 	router := mux.NewRouter()
 	router.Use(logMiddleware.LogRequests)
 	router.Use(authMiddleware.EnsureAuth)
 
-	router.HandleFunc("/", homeHandler.ServeHome).Methods("GET")
-	router.HandleFunc("/auth", authHandler.RootHandler).Methods("GET")
-	router.HandleFunc("/auth/google/callback", authHandler.GoogleCallbackHandler).Methods("GET")
-	router.HandleFunc("/user", userHandler.List)
-	router.HandleFunc("/user/new", userHandler.Enroll)
-	router.HandleFunc("/user/{email}", userHandler.View)
-	router.HandleFunc("/user/{email}/password", userHandler.Renew)
-	router.HandleFunc("/user/{email}/delete", userHandler.Delete)
+	router.HandleFunc("/", homeHandler.Root).Methods(http.MethodGet)
+	router.HandleFunc("/auth/", authHandler.RootHandler).Methods(http.MethodGet)
+	router.HandleFunc("/auth/google/callback", authHandler.GoogleCallbackHandler).Methods(http.MethodGet)
+	router.HandleFunc("/user/", userHandler.List).Methods("GET")
+	router.HandleFunc("/user/{email}/", userHandler.View).Methods(http.MethodGet, http.MethodDelete)
+	router.HandleFunc("/user/{email}/enroll", userHandler.Enroll).Methods(http.MethodGet)
+	router.HandleFunc("/user/{email}/password", userHandler.Renew).Methods(http.MethodGet)
+	router.HandleFunc("/user/{email}/delete", userHandler.Delete).Methods(http.MethodGet)
 	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.FS(assets))))
+
+	router.NotFoundHandler = http.HandlerFunc(homeHandler.NotFound)
 	http.Handle("/", router)
 
 	log.Printf("Created httpd server on :9990")
